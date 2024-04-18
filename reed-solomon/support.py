9ada81a220eaf4b8e5b34800451bfdfdd697ce4c
mohit086 <155368106+mohit086@users.noreply.github.com>
2024-04-18 21:54:38 +0530

    modified some functions

--- reed-solomon/support.py
+++ reed-solomon/support.py
@@ -1,5 +1,4 @@
 import gmpy2
-
 rs = gmpy2.random_state(hash(gmpy2.random_state()))
 
 # Converts a string to a mpz bigint object
@@ -12,21 +11,32 @@ def rnd(a, b):
 
 # Returns the remainder when a is divided by b
 def mod(a, b):
-    ans = a - b*(a//b)
+    ans = a - b * (a // b)
     if ans < 0 and b > 0:
         ans += b
     if ans < 0 and b < 0:
         ans -= b
     return ans
 
-def pown(a,b,m):
-    a = mod(a,m)
+# Returns a^b
+def power(a, b):
     ans = 1
     while b > 0:
         if b & 1:
-            ans = mod(ans * a , m)
-        a = mod(a * a , m)
-        b //=2
+            ans = ans * a
+        a = a * a
+        b //= 2
+    return ans
+
+# Returns (a^b) mod n
+def powermod(a, b, n):
+    a = mod(a, n)
+    ans = 1
+    while b > 0:
+        if b & 1:
+            ans = mod(ans * a , n)
+        a = mod(a * a , n)
+        b //= 2
     return ans
 
 # Returns the GCD d of 2 positive integers a and b, and integers s and t such that as + bt = d
@@ -39,54 +49,53 @@ def egcd(a, b):
         while mod(r, 2) == 0:
             r //= 2
             if mod(s, 2) == 0 and mod(t, 2) == 0:
-                s, t = s//2, t//2
+                s, t = s // 2, t // 2
             else:
-                s, t = (s + b_dash)//2, (t - a_dash)//2
+                s, t = (s + b_dash) // 2, (t - a_dash) // 2
         while mod(r_dash, 2) == 0:
-            r_dash = r_dash//2
+            r_dash //= 2
             if mod(s_dash, 2) == 0 and mod(t_dash, 2) == 0:
-                s_dash, t_dash = s_dash//2, t_dash//2
+                s_dash, t_dash = s_dash // 2, t_dash // 2
             else:
-                s_dash, t_dash = (s_dash + b_dash)//2, (t_dash - a_dash)//2
+                s_dash, t_dash = (s_dash + b_dash) // 2, (t_dash - a_dash) // 2
         if r_dash < r:
             r, s, t, r_dash, s_dash, t_dash = r_dash, s_dash, t_dash, r, s, t
-        r_dash, s_dash, t_dash = r_dash-r, s_dash-s, t_dash-t
-        d = r << e
-    return [d,s,t]
+        r_dash, s_dash, t_dash = r_dash - r, s_dash - s, t_dash - t
+        d = r *power(2, e)
+    return [d, s, t]
 
 # Implementation of CRT using EGCD for the inverse
 def crt(n_list, a_list):
     N, x = 1, 0
     N_list, N_bar_list = [], []
     for n in n_list:
-        N = N*n
+        N = N * n
     for n in n_list:
-        Ni = N//n
+        Ni = N // n
         N_list.append(Ni)
         temp = egcd(Ni, n)[1]
         if(temp < 0) :
             temp = mod(egcd(Ni, n)[1], n)
         N_bar_list.append(temp)
     for i in range(len(n_list)):
-        x += a_list[i]*n_list[i]*N_bar_list[i]
+        x += a_list[i] * n_list[i] * N_bar_list[i]
     return mod(x, N)
 
-# Miller-Rabin test for checking primality
+# Miller - Rabin test for checking primality
 def miller_rabin(n, rounds):
     if n in [2, 3]:
         return True
-    if n==1 or mod(n,2) == 0:
+    if n == 1 or mod(n, 2) == 0:
         return False
-    t = n - 1
-    h = 1
-    while mod(t,2) == 0:
+    t, h = n - 1, 1
+    while mod(t, 2) == 0:
         t = t // 2
         h += 1
     for _ in range(rounds):
         a = rnd(2, n-1)
-        x = pown(a, t, n)
+        x = powermod(a, t, n)
         for _ in range(h):
-            y = pown(x, 2, n)
+            y = powermod(x, 2, n)
             if y == 1 and x != 1 and x != n-1:
                 return False
             x = y
