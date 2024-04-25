ce8e51c1d7096ac56c4ead28d40d8c4e3748fd5b
Ananthakrishna-K-13 <Ananthakrishna.K@iiitb.ac.in>
2024-04-25 11:14:56 +0530

    improved accuracy

--- reed-solomon/reed_solomon.py
+++ reed-solomon/reed_solomon.py
@@ -3,34 +3,29 @@ import random
 import math
 from support import *
 
-# M = int(input())
-# mu = int(input())
-# k = int(input())
 
-M=1000000000000000000
-mu = 0.4
-k = 12
+M=gmpy2.mpz('1'+'0'*1000)
+mu = random.random()
+# mu = 0.43
+k = 1000
 
 primes = []
 currupted_residues = mu*k
 
 
-
 def GlobalSetup(mu, M):
     global primes
-    primes = getkprimes(math.sqrt(M), M,k)
+    primes = getkprimes(k)
 
 
 def ReedSolomonSend(a):
-    tosend = [ a%pi for pi in primes]
+    tosend = [ mod(a,pi) for pi in primes]
     b = Transmit(tosend)
-    # print(tosend)
     return b
 
 
 def Transmit(tosend):
     I = random.sample(range(k),random.randint(0,int(currupted_residues))) 
-    print(I)
     b = []
     for i in range(k):
         if(i in I):
@@ -42,11 +37,10 @@ def Transmit(tosend):
             b.append(tosend[i])
     return b
 
+
 def ReedSolomonReceive(b):
+
     tempb = crt(primes,b)
-    # print(primes)
-    # print(b)
-    # print(tempb)
 
     primes.sort(reverse=True)
     P = 1
@@ -55,40 +49,47 @@ def ReedSolomonReceive(b):
         if(i<currupted_residues):
             P=P*primes[i]
         n = n * primes[i]
-    assert (n > 2*M*P*P)
+
+    if(n <= 2*M*P*P):
+        return(-1)
 
     rstar = M*P
-
     lists = extgcd(n,tempb)
-    # print(lists[0])
-    # print(rstar)
-    # print(lists[1])
-    # print(lists[2])
-    # print(rstar)
 
     ind = 0
     while(lists[0][ind]>rstar):
         ind = ind+1
 
     rdash,sdash,tdash = lists[0][ind],lists[1][ind],lists[2][ind]
-    # mod(tdash,n)
-
-    # n,tempb,rstr,P  ==> rdash,sdash,t_dash
-
 
     if(rdash%tdash == 0):
         return(rdash//tdash)
     else:
-        return("Error")
+        return(-1)
     
+def main():
+    GlobalSetup(mu,M)
+    a = random.randint(0,M)
+    print("Message transmitted:")
+    print(a)
+    stat = ReedSolomonReceive(ReedSolomonSend(a))
+    # print(stat)
+    if(stat == -1):
+        print(mu)
+        print("Message could not be reconstructed")
+    else:
+        print("Message recieved:")
+        print(stat)
+
+
+main()
 
 
 
 
 
 
-GlobalSetup(mu,M)
-print(ReedSolomonReceive(ReedSolomonSend(34664363434737)))
+
 
 
 
